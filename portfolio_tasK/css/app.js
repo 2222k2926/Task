1e1f23b19ef83651ab32937235a6ba1146173c15
2222k2926 <144605049+2222k2926@users.noreply.github.com>
2024-07-14 20:24:35 +0530

    Add files via upload

---
+++ portfolio_tasK/css/app.js
@@ -0,0 +1,33 @@
+var petDog = "Rex";
+var petCat = "Pepper";
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is:", petDog);
+console.log("My pet dog's name is:", petCat);
+var catSound = "purr";
+var dogSound = "woof";
+console.log(petDog,"says",dogSound);
+console.log(petCat,"says",catSound);
+catSound = "meow";
+console.log(petCat,"now says",catSound);
+var geo = "coimbatore";
+var job = "Front-end Developer";
+console.log("you will be a",job,"in",geo);
+let obj={
+    FirstName: "Angel Mary",
+    LastName: "Ali Akbar",
+    Age: 19
+};
+console.log(obj);
+var BirthYear = 2004;
+var CalYear = 2030;
+var futureAge = CalYear - BirthYear;
+console.log(futureAge);
+var currentAge=20;
+var MaxAge=50;
+var PerDay=4;
+var RestValues= MaxAge - currentAge;
+var RestDay = RestValues * 365;
+var RestFav = RestDay * 4;
+console.log("I need " +RestFav+ " of Chocolate until the old age of "+ MaxAge);
+
